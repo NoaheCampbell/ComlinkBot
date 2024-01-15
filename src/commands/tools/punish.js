7be0f082f74ae344faf17bee0945ff84e058df5b
noah <noah.edward.campbell@gmail.com>
2024-01-15 16:17:52 -0700

    Changed user to ign
    Changed punish to take a string for a player's IGN instead of a discord user, and also made the duration a string to account for permanent options

--- src/commands/tools/punish.js
+++ src/commands/tools/punish.js
@@ -3,9 +3,9 @@ const { Client, GatewayIntentBits, PermissionFlagsBits, Partials, SlashCommandBu
 module.exports = {
     data: new SlashCommandBuilder()
         .setName("punish")
-        .setDescription("Punish a user")
-        .addUserOption(option => 
-            option.setName("user")
+        .setDescription("Log a player punishment")
+        .addStringOption(option => 
+            option.setName("ign")
             .setDescription("The user to punish")
             .setRequired(true)
         )
@@ -15,18 +15,18 @@ module.exports = {
             .setRequired(true)
             .addChoices({ name: "Warn", value: "warn"}, { name: "Mute", value: "mute"}, { name: "Ban", value: "ban"})
         )
-        .addIntegerOption(option =>
-            option.setName("duration")
-            .setDescription("The duration of the punishment")
-            .setRequired(false)
-        )
         .addStringOption(option => 
             option.setName("reason")
             .setDescription("The reason for the punishment")
             .setRequired(true)
+        )
+        .addStringOption(option =>
+            option.setName("duration")
+            .setDescription("The duration of the punishment (e.g., '2h', '4d', '1w', 'permanent')")
+            .setRequired(true)
         ),
 
     async execute(interaction) {
-        interaction.reply(`${interaction.options.getString("type")} ${interaction.options.getUser("user")} for ${interaction.options.getString("duration")} for ${interaction.options.getString("reason")}`);
+        interaction.reply(`${interaction.options.getString("type")} ${interaction.options.getString("ign")} for ${interaction.options.getString("duration")} for ${interaction.options.getString("reason")}`);
     }
 }
